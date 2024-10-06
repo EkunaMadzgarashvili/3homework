63e2341313243596f7e9bd3b273cf76691eedf56
Ekuna Madzgarashvili <ekunamadzgarashvili1@gmail.com>
2024-10-06 21:22:40 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ekaterine Madzgharashvili");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine(Console.ReadLine());
